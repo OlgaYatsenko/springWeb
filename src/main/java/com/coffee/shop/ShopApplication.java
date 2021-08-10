30fd2af5839718b02a89836e40535fbef8d90089
OlgaYatsenko <myjob144@gmail.com>
2021-08-10 15:18:01 +0200

    resolving issue with order submission

--- src/main/java/com/coffee/shop/ShopApplication.java
+++ src/main/java/com/coffee/shop/ShopApplication.java
@@ -6,11 +6,14 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ShopApplication {
+public class ShopApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);}
 
-
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
 
 }
